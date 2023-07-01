8c84364cad79a28e89118faa1fc82b204a5e8644
MD.Kamrul Hasan <kamrulhasan635653@gmail.com>
2023-07-01 17:04:43 +0600

    project setup

---
+++ src/config/index.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+export default {
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+};
+//vh5svV2BM013i7mp
