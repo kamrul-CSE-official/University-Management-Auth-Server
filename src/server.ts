97b02e1585d622b3ad1fb8fba32ab2a8d42bf9ef
MD.Kamrul Hasan <kamrulhasan635653@gmail.com>
2023-07-02 13:17:36 +0600

    update

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,17 @@
-console.log("Bismillah");
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+async function boostrap() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log(`👏 Database connected successfully.`);
+
+    app.listen(config.port, () => {
+      console.log(`Application running on `, config.port);
+    });
+  } catch (error) {
+    console.log("Failed to connect data,", error);
+  }
+}
+boostrap();
